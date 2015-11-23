6db5aca8c646986be3c8fb7b04f61e9fe175e33b
Vikash Loomba <loomba.vikash@gmail.com>
2015-11-23 14:54:16 -0800

    Added search functionality to the list view controller

--- nav_test/DispensariesListViewController.swift
+++ nav_test/DispensariesListViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 import Alamofire
 
 
-class DispensariesListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class DispensariesListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating, UISearchBarDelegate{
     
     //Search bar variable
 
 
-    @IBOutlet weak var searchBar: UISearchBar!
-
+//    @IBOutlet weak var searchBar: UISearchBar!
+    
+    //Test and delete
+    var resultSearchController = UISearchController()
     
     
     //Table Variables
@@ -29,7 +31,15 @@ class DispensariesListViewController: UIViewController, UITableViewDelegate, UIT
     override func viewDidLoad() {
         super.viewDidLoad()
         requestDispensaries()
-        self.searchBar.sizeToFit()
+        //Test and delete
+        self.resultSearchController = UISearchController(searchResultsController: nil)
+        self.resultSearchController.searchResultsUpdater = self
+        self.resultSearchController.dimsBackgroundDuringPresentation = false
+        self.resultSearchController.hidesNavigationBarDuringPresentation = false
+        self.resultSearchController.searchBar.sizeToFit()
+        self.tableView.tableHeaderView = self.resultSearchController.searchBar
+        
+
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView.reloadData();
@@ -88,53 +98,106 @@ class DispensariesListViewController: UIViewController, UITableViewDelegate, UIT
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
+        if (self.resultSearchController.active) {
+            return self.filteredDispensaries.count
+        } else {
+            return self.dispensaries.count
+        }
         
-        return self.dispensaries.count
+
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell : DispensaryCell
-        cell = tableView.dequeueReusableCellWithIdentifier("DispensaryCell", forIndexPath: indexPath) as! DispensaryCell
-        cell.dispensaryName!.text = dispensaries[indexPath.row].name
-        cell.dispensaryPhone!.text = dispensaries[indexPath.row].phone
-        cell.dispensaryStreetAddress!.text = dispensaries[indexPath.row].address
-        cell.dispensaryCityState!.text = dispensaries[indexPath.row].city.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()) + ", " + dispensaries[indexPath.row].state
-        let request: NSURLRequest = NSURLRequest(URL: NSURL(string: dispensaries[indexPath.row].logo)!)
-        let mainQueue = NSOperationQueue.mainQueue()
-        NSURLConnection.sendAsynchronousRequest(request, queue: mainQueue, completionHandler: { (response, data, error) -> Void in
-            if error == nil {
-                // Convert the downloaded data in to a UIImage object
-                // Update the cell
-                dispatch_async(dispatch_get_main_queue(), {
-                    if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath) {
-                        cell.dispensaryLogo.image = UIImage(data: data!)
-                    }
-                })
-            }
-            else {
-                print("Error: \(error!.localizedDescription)")
-            }
-        })
         
-        return cell
+        if (self.resultSearchController.active) {
+            cell = tableView.dequeueReusableCellWithIdentifier("DispensaryCell", forIndexPath: indexPath) as! DispensaryCell
+            cell.dispensaryName!.text = filteredDispensaries[indexPath.row].name
+            cell.dispensaryPhone!.text = filteredDispensaries[indexPath.row].phone
+            cell.dispensaryStreetAddress!.text = filteredDispensaries[indexPath.row].address
+            cell.dispensaryCityState!.text = dispensaries[indexPath.row].city.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()) + ", " + filteredDispensaries[indexPath.row].state
+            let request: NSURLRequest = NSURLRequest(URL: NSURL(string: filteredDispensaries[indexPath.row].logo)!)
+            let mainQueue = NSOperationQueue.mainQueue()
+            NSURLConnection.sendAsynchronousRequest(request, queue: mainQueue, completionHandler: { (response, data, error) -> Void in
+                if error == nil {
+                    // Convert the downloaded data in to a UIImage object
+                    // Update the cell
+                    dispatch_async(dispatch_get_main_queue(), {
+                        if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath) {
+                            cell.dispensaryLogo.image = UIImage(data: data!)
+                        }
+                    })
+                }
+                else {
+                    print("Error: \(error!.localizedDescription)")
+                }
+            })
+            
+            return cell
+            
+        } else {
+            
+            cell = tableView.dequeueReusableCellWithIdentifier("DispensaryCell", forIndexPath: indexPath) as! DispensaryCell
+            cell.dispensaryName!.text = dispensaries[indexPath.row].name
+            cell.dispensaryPhone!.text = dispensaries[indexPath.row].phone
+            cell.dispensaryStreetAddress!.text = dispensaries[indexPath.row].address
+            cell.dispensaryCityState!.text = dispensaries[indexPath.row].city.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet()) + ", " + dispensaries[indexPath.row].state
+            let request: NSURLRequest = NSURLRequest(URL: NSURL(string: dispensaries[indexPath.row].logo)!)
+            let mainQueue = NSOperationQueue.mainQueue()
+            NSURLConnection.sendAsynchronousRequest(request, queue: mainQueue, completionHandler: { (response, data, error) -> Void in
+                if error == nil {
+                    // Convert the downloaded data in to a UIImage object
+                    // Update the cell
+                    dispatch_async(dispatch_get_main_queue(), {
+                        if let cellToUpdate = tableView.cellForRowAtIndexPath(indexPath) {
+                            cell.dispensaryLogo.image = UIImage(data: data!)
+                        }
+                    })
+                }
+                else {
+                    print("Error: \(error!.localizedDescription)")
+                }
+            })
+            
+            return cell
+        }
+        
+        
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         dispatch_async(dispatch_get_main_queue()) {
             self.performSegueWithIdentifier("ShowMenu", sender: self.tableView.cellForRowAtIndexPath(indexPath))
         }
+        
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let menuViewController = segue.destinationViewController as! MenuViewController
         if let indexPath = tableView.indexPathForCell(sender as! UITableViewCell) {
-            menuViewController.dispensary = dispensaries[indexPath.row]
+            if (self.resultSearchController.active) {
+                menuViewController.dispensary = filteredDispensaries[indexPath.row]
+            } else {
+                menuViewController.dispensary = dispensaries[indexPath.row]
+            }
+            
         }
+        self.resultSearchController.active = false
         
     }
-
-
     
+
+    //Search results controller
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        print(self.resultSearchController.active)
+        self.filteredDispensaries.removeAll(keepCapacity: false)
+        
+        let searchPredicate = NSPredicate(format: "SELF.title CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (self.dispensaries as NSArray).filteredArrayUsingPredicate(searchPredicate)
+        self.filteredDispensaries = array as! [mkDispensary]
+        
+        self.tableView.reloadData()
+    }
     
     
 }
